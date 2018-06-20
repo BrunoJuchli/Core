c8f9326c97fc4d1d92f8ad6fe0a69da07e428273
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2018-06-20 17:09:57 +0200

    todos

--- src/NetCoreRepro/Todos.cs
+++ src/NetCoreRepro/Todos.cs
@@ -6,7 +6,8 @@
 	 *		--> ProxyGenerationOptions: is not immutable but is used as part of dictionary key. What's worse, ProxyGenerationOptions.Default is a "global" instance that is
 	 *		used per default but anyone can change.
 	 *
-	 *
+	 *	Rewrite ModuleScope, NamingScope so reading operations are only available on Read-Lock-Scope-Interface
+	 *	and Writing operations are only available on Write-Lock-Scope-Interface
 	 *
 	 *
 	 */
