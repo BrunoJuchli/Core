d38e870983ab3a30c7cde094950508f35d4813ee
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2018-06-25 17:31:53 +0200

    using a new ProxyGenerator per proxy guarantees creating a new module per proxy

--- src/NetCoreRepro/ProxyFactory.cs
+++ src/NetCoreRepro/ProxyFactory.cs
@@ -1,34 +1,33 @@
-﻿using Castle.DynamicProxy;
-using System;
+﻿using System;
+using Castle.DynamicProxy;
 
 namespace NetCoreRepro
 {
-    public static class ProxyFactory
-    {
-        static ProxyGenerator generator = new ProxyGenerator();
-        static Interceptor interceptor = new Interceptor();
+	public static class ProxyFactory
+	{
+		static readonly Interceptor interceptor = new Interceptor();
 
-        public static T CreateProxy<T>() where T : class
-        {
-            return generator.CreateInterfaceProxyWithoutTarget<T>(interceptor);
-        }
+		public static T CreateProxy<T>() where T : class
+		{
+			return new ProxyGenerator().CreateInterfaceProxyWithoutTarget<T>(interceptor);
+		}
 
-        private sealed class Interceptor : IInterceptor
-        {
-            public void Intercept(IInvocation invocation)
-            {
-                if (invocation.Method.ReturnType == typeof(void))
-                {
-                }
-                else if (invocation.Method.ReturnType == typeof(bool))
-                {
-                    invocation.ReturnValue = true;
-                }
-                else
-                {
-                    invocation.ReturnValue = Activator.CreateInstance(invocation.Method.ReturnType);
-                }
-            }
-        }
-    }
+		private sealed class Interceptor : IInterceptor
+		{
+			public void Intercept(IInvocation invocation)
+			{
+				if (invocation.Method.ReturnType == typeof(void))
+				{
+				}
+				else if (invocation.Method.ReturnType == typeof(bool))
+				{
+					invocation.ReturnValue = true;
+				}
+				else
+				{
+					invocation.ReturnValue = Activator.CreateInstance(invocation.Method.ReturnType);
+				}
+			}
+		}
+	}
 }
